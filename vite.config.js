cd69bb88321573391b6c212af0156d17e302912f
Rohit-Doi <rohitkamatam123@gmail.com>
2026-05-15 16:58:50 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',  // ← Replace with your exact GitHub repo name
+  base: Portfolio,  // ← Replace with your exact GitHub repo name
 })
